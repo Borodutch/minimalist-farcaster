9e86952305fe3183361c9ebbd9dbeb649c1aee7d
Nikita Kolmogorov <backmeupplz@gmail.com>
2024-05-03 12:35:58 -0700

    remove boilerplate

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -6,11 +6,4 @@ import { resolve } from 'path'
 dotenv.config({ path: resolve(cwd(), '.env') })
 
 // eslint-disable-next-line node/no-process-env
-export default cleanEnv(process.env, {
-  FACEBOOK_APP_ID: str(),
-  FACEBOOK_APP_SECRET: str(),
-  JWT: str(),
-  MONGO: str(),
-  PORT: num({ default: 1337 }),
-  TELEGRAM_LOGIN_TOKEN: str(),
-})
+export default cleanEnv(process.env, {})
